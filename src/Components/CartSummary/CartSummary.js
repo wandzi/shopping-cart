464507a552972fe1bf74cbc5d35ce56b3bd4ef68
wandzi <wandzel.mateusz@yahoo.com>
2020-10-23 20:35:49 +0200

    Feat(ShoppingCart): User interface is almost complete - going to implement functionality.

---
+++ src/Components/CartSummary/CartSummary.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import './CartSummary.scss'
+
+const CartSummary = () => {
+    return (
+        <div className="cart-summar-container">
+            <div className="cart-summary__shipping">
+                <span>Shipping:</span> 
+                <span>$23.80</span>    
+            </div>
+
+            <div className="cart-total-container">
+                <div className="cart-summary__total">Cart totals</div>
+                <div className="cart-summary__description"> 
+                    <span className="cart-summary__title">Subtotal</span> 
+                    <span>$23.80</span>
+                </div>
+                <div className="cart-summary__description">
+                    <span className="cart-summary__title">Grand total</span> 
+                    <span>$23.80</span>
+                </div>
+                <button className="cart-total__button ">Proceed to checkout</button>
+            </div>
+        </div>
+    ) 
+}
+
+export default CartSummary;
--- src/Components/ShoppingCart/ShoppingCart.js
+++ src/Components/ShoppingCart/ShoppingCart.js
@@ -3,13 +3,17 @@ import './ShoppingCart.scss'
 
 import Header from '../Header/Header'
 import ShoppingCartTable from '../ShoppingCartTable/ShoppingCartTable'
+import CartSummary from '../CartSummary/CartSummary'
 
 const ShoppingCart = () => {
     return (
         <div className="shopping-cart-container">
             <Header />
-            <div className="shopping-cart-container__main-shopping-cart">
-                <ShoppingCartTable />
+            <div className="shopping-cart-container__grid">
+                <div className="shopping-cart-main">
+                    <ShoppingCartTable />
+                </div>
+                <CartSummary />
             </div>
         </div>
     )
